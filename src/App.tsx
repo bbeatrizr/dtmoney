b282ad739b9223f5a3f03a0f6ca64c2c4381ead9
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2022-07-06 19:18:39 -0300

    feat: 🎸 modal criation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        onRequestClose={handleCloseNewTransactionModal}
+        isOpen={isNewTransactionModalOpen}
+      />
       <GlobalStyle />
     </>
   );
